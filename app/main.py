d659cfdf101dd076a8fa8ef236e7142c8f9f6897
Atgoogat <36623640+Atgoogat@users.noreply.github.com>
2022-11-02 22:07:34 +0100

    add: env var option for root path

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.routing import APIRouter
 import app.swapi as swapi
 
-app = FastAPI()
+from os import environ
 
-@app.get("/")
+
+router = APIRouter()
+
+
+@router.get("/")
 async def root():
     return swapi.getCanteenData()
+
+app = FastAPI()
+app.include_router(router, prefix=environ.get('OMKA_ROOT_PATH') or "")
