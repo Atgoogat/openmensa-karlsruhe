a8558123682ccb7cf501003a03c4571f4151dec4
Atgoogat <36623640+Atgoogat@users.noreply.github.com>
2022-11-03 12:14:53 +0100

    fix: correct 404 behavior

--- app/main.py
+++ app/main.py
@@ -16,6 +16,9 @@ async def root():
 @router.get("/feed/{canteen}")
 async def feed(canteen: str):
     canteenData = swapi.getCanteenData()
+    if canteenData.get(canteen) is None:
+        return Response(status_code=404)
+        
     c = swapi.toCanteen(canteen, canteenData)
     return Response(content=generateFeedV2(c, "0.1-a"), media_type="application/xml")
 
