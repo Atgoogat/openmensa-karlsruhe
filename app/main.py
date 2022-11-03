d6709b0e437fac25da7f58518e93ee3815c377a7
Atgoogat <36623640+Atgoogat@users.noreply.github.com>
2022-11-03 12:43:40 +0100

    add: meta endpoint

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Response
 from fastapi.routing import APIRouter
 import app.swapi as swapi
 from app.feedv2.feed import generateFeedV2
+from app.feedv2.meta import getOpenMensaMetaData
 
 from os import environ
 
@@ -27,6 +28,14 @@ async def feed(canteen: str):
         
     c = swapi.toCanteen(canteen, canteenData, lines)
     return Response(content=generateFeedV2(c, "0.1-a"), media_type="application/xml")
+
+@router.get("/meta/{canteen}")
+async def meta(canteen: str):
+    metaData = getOpenMensaMetaData(canteen)
+    if metaData is None:
+        return Response(status_code=404)
+    return Response(content=metaData, media_type="application/xml")
+
 
 app = FastAPI()
 app.include_router(router, prefix=environ.get('OMKA_ROOT_PATH') or "")
