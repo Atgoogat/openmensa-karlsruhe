a4bbbb7dce4ed60b098c05eed948580038d7cc65
Atgoogat <36623640+Atgoogat@users.noreply.github.com>
2023-06-06 00:22:51 +0200

    add: support for new json api

--- app/main.py
+++ app/main.py
@@ -1,39 +1,37 @@
-from bs4 import BeautifulSoup
 from fastapi import FastAPI, Response
 from fastapi.routing import APIRouter
-import app.swapi as swapi
-from app.feedv2.feed import Canteen, generateFeedV2
+
+import app.mensaakkapi as mensaakkapi 
+from app.feedv2.feed import generateFeedV2
 from app.feedv2.meta import getOpenMensaMetaData
 
 from os import environ
+from datetime import datetime, timedelta
 
 
 router = APIRouter()
 
+@router.get("/feed/adenauerring")
+async def feedAdenauerring():
+    mensaDays = []
+    date = datetime.today()
+    # fetch next 10 days
+    for i in range(10):
+        day = mensaakkapi.getMensaAdenauerringMeals(date + timedelta(days=i))
+        mensaDays.append(day)
 
-@router.get("/")
-async def root():
-    canteenSoup = swapi.getCanteenHtml()
-    if canteenSoup is None:
-        return Response(status_code=404)
+    canteenData = mensaakkapi.toCanteen(mensaDays)
+    return Response(content=generateFeedV2(canteenData, "0.1-a"), media_type="application/xml")
 
-    return generateFeedV2(swapi.getCanteen(canteenSoup), "0.1-a")
-    
+@router.get("/feed/adenauerring/{date}")
+async def feedAdenauerring(date: str):
+    data = mensaakkapi.getMensaAdenauerringMeals(datetime.strptime(date, "%Y-%m-%d"))
+    canteenData = mensaakkapi.toCanteen([data])
+    return Response(content=generateFeedV2(canteenData, "0.1-a"), media_type="application/xml")
 
-@router.get("/feed/{canteen}")
-async def feed(canteen: str):
-    canteen: Canteen
-    canteenSoup = swapi.getCanteenHtml()
-    if canteenSoup is None:
-        return Response(status_code=404)
-
-    canteen = swapi.getCanteen(canteenSoup)
-
-    return Response(content=generateFeedV2(canteen, "0.1-a"), media_type="application/xml")
-
-@router.get("/meta/{canteen}")
-async def meta(canteen: str):
-    metaData = getOpenMensaMetaData(canteen)
+@router.get("/meta/adenauerring")
+async def meta():
+    metaData = getOpenMensaMetaData("adenauerring")
     if metaData is None:
         return Response(status_code=404)
     return Response(content=metaData, media_type="application/xml")
